04f2d44ae651f31db2789478428a3f7ad831dc79
Matt H <mhh129@gmail.com>
2024-07-26 14:01:21 -0400

    Update cocktail-details stylesheet

--- src/app/components/cocktail-card/cocktail-card.component.ts
+++ src/app/components/cocktail-card/cocktail-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject, Input, OnInit } from '@angular/core';
+import { Component, inject, Input } from '@angular/core';
 import { Cocktail } from '../../shared/interfaces/cocktail';
 import { CommonModule } from '@angular/common';
 import { AlcoholicPipe } from '../../shared/pipes/alcoholic.pipe';
@@ -12,13 +12,8 @@ import { RouterModule } from '@angular/router';
   templateUrl: './cocktail-card.component.html',
   styleUrl: './cocktail-card.component.scss'
 })
-export class CocktailCardComponent implements OnInit{
+export class CocktailCardComponent {
   @Input() cocktailData: Cocktail;
 
   protected favoritesService = inject(FavoritesService);
-
-  ngOnInit(): void {
-    // console.log('--- cocktailData:', this.cocktailData)
-  }
-
 }
--- src/app/components/cocktail-details/cocktail-details.component.ts
+++ src/app/components/cocktail-details/cocktail-details.component.ts
@@ -1,12 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, inject, Input } from '@angular/core';
+import { Observable, tap } from 'rxjs';
+import { Cocktail } from '../../shared/interfaces/cocktail';
+import { HttpClient } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
+import { AlcoholicPipe } from '../../shared/pipes/alcoholic.pipe';
+import { RouterModule } from '@angular/router';
+import { FavoritesService } from '../../services/favorites/favorites.service';
 
 @Component({
   selector: 'app-cocktail-details',
   standalone: true,
-  imports: [],
+  imports: [ AlcoholicPipe, CommonModule, RouterModule ],
   templateUrl: './cocktail-details.component.html',
   styleUrl: './cocktail-details.component.scss'
 })
 export class CocktailDetailsComponent {
+  @Input() cocktailId: any;
 
+  private http = inject(HttpClient);
+
+  protected favoritesService = inject(FavoritesService);
+  protected cocktailDetails$: Observable<Cocktail>;
+
+  ngOnInit(): void {
+    console.log('--- this.cocktailId:', this.cocktailId)
+    this.cocktailDetails$ = this.http.get<Cocktail>(`/cockails/${this.cocktailId}`).pipe(
+      tap(response => {
+
+        console.log('--- response:', response)
+        
+        // this.cocktails = response;
+        // this.cocktailsSignal.set(response);
+      })
+    );
+  }
+
+  onBack(): void {
+    history.back();
+  }
 }
