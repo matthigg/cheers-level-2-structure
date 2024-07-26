48d6a4894911da3bec8726a5b0bdfe8b0d3d13dd
Matt H <mhh129@gmail.com>
2024-07-26 09:46:03 -0400

    Update cocktail-card component to display cocktail data

--- src/app/components/cocktail-card/cocktail-card.component.ts
+++ src/app/components/cocktail-card/cocktail-card.component.ts
@@ -1,12 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Cocktail } from '../../shared/interfaces/cocktail';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-cocktail-card',
   standalone: true,
-  imports: [],
+  imports: [ CommonModule ],
   templateUrl: './cocktail-card.component.html',
   styleUrl: './cocktail-card.component.scss'
 })
-export class CocktailCardComponent {
+export class CocktailCardComponent implements OnInit{
+  @Input() cocktailData?: Cocktail;
+
+  ngOnInit(): void {
+    console.log('--- cocktailData:', this.cocktailData)
+  }
 
 }
--- src/app/components/homepage/homepage.component.ts
+++ src/app/components/homepage/homepage.component.ts
@@ -3,11 +3,12 @@ import { Component, inject } from '@angular/core';
 import { Observable, tap } from 'rxjs';
 import { Cocktail } from '../../shared/interfaces/cocktail';
 import { CommonModule } from '@angular/common';
+import { CocktailCardComponent } from "../cocktail-card/cocktail-card.component";
 
 @Component({
   selector: 'app-homepage',
   standalone: true,
-  imports: [ CommonModule ],
+  imports: [CommonModule, CocktailCardComponent],
   templateUrl: './homepage.component.html',
   styleUrl: './homepage.component.scss'
 })
