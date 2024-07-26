b4e7d0220f91b90eb2caab0648ef96d97b928fd3
Matt H <mhh129@gmail.com>
2024-07-26 09:34:18 -0400

    Create Cocktails interface & homepage component cocktails$ property

--- src/app/components/homepage/homepage.component.ts
+++ src/app/components/homepage/homepage.component.ts
@@ -1,26 +1,39 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, inject } from '@angular/core';
+import { Observable, tap } from 'rxjs';
+import { Cocktail } from '../../shared/interfaces/cocktail';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-homepage',
   standalone: true,
-  imports: [],
+  imports: [ CommonModule ],
   templateUrl: './homepage.component.html',
   styleUrl: './homepage.component.scss'
 })
 export class HomepageComponent {
   private http = inject(HttpClient);
 
+  protected cocktails$: Observable<Cocktail[]>;
+
   ngOnInit(): void {
-    this.http.get('/cockails').subscribe(response => {
-      console.log('--- response:', response)
-    })
+    // this.http.get<Cocktail[]>('/cockails').subscribe((response: Cocktail[]) => {
+    //   console.log('--- response:', response)
+    //   this.cocktails$ = response
+    // })
+
+    this.cocktails$ = this.http.get<Cocktail[]>('/cockails')
+      .pipe(
+        tap(response => {
+          console.log('--- response:', response)
+        })
+      )
   }
 
   onFilter(value?: string): void {
 
     console.log('--- value:', value)
-    
+
   }
 
 }
