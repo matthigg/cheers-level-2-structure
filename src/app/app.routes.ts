6281c0bac41c981b9ecbea437c139e93e7045909
Matt H <mhh129@gmail.com>
2024-07-26 12:56:54 -0400

    Update cocktail-card stylesheet

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { HomepageComponent } from './components/homepage/homepage.component';
+import { CocktailDetailsComponent } from './components/cocktail-details/cocktail-details.component';
 
 export const routes: Routes = [
   { path: '', component: HomepageComponent },
+  { path: 'cocktails/:cocktailId', component: CocktailDetailsComponent },
 ];
--- src/app/components/cocktail-card/cocktail-card.component.ts
+++ src/app/components/cocktail-card/cocktail-card.component.ts
@@ -3,11 +3,12 @@ import { Cocktail } from '../../shared/interfaces/cocktail';
 import { CommonModule } from '@angular/common';
 import { AlcoholicPipe } from '../../shared/pipes/alcoholic.pipe';
 import { FavoritesService } from '../../services/favorites/favorites.service';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-cocktail-card',
   standalone: true,
-  imports: [ AlcoholicPipe, CommonModule ],
+  imports: [ AlcoholicPipe, CommonModule, RouterModule ],
   templateUrl: './cocktail-card.component.html',
   styleUrl: './cocktail-card.component.scss'
 })
